0b418cda8d40d73ceb4e08df6b67143d63b970af
62R <dima1u@mail.ru>
2022-12-18 17:50:16 +0300

    создал компонент message

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import { Count as CountClass } from './class-components/Count';
-import { Form as FormClass } from './class-components/Form';
-import { Child } from './func-components/Child';
-import { Count } from './func-components/Count';
-import { Form } from './func-components/Form';
+import { Message } from './func-components/Message';
 
 export const App = () => {
-  const [name, setName] = useState('geek')
-  const [count, setCount] = useState(0)
-
-
-  const handleChangeName = (ev) => {
-    setName(ev.target.value)
-  }
+  const [message, setMessage] = useState('some message')
 
   return (
     <div className="App">
-      {/* <Form />
-      <hr />
-      <Count name={'geekbrains'} />
-      <hr /> */}
-
-      {/* <hr /><hr />
-      <CountClass count={10} />
-      <hr />
-      <FormClass /> */}
-
-      <input type="text" onChange={handleChangeName} />
-      <p>{count}</p>
-      <Child name={name} handleChangeCount={setCount} />
+      <Message text={message} />
     </div>
   );
 }
---
+++ src/func-components/Message.jsx
@@ -0,0 +1,4 @@
+export const Message = ({ text }) =>
+    <div>
+        <p>{text}</p>
+    </div>
